7181a7bccf2a677fe785b19eed13ed2c81efe77b
drpriver <david@davidpriver.com>
2026-01-02 10:34:20 -0800

    bench stuff

--- Makefile
+++ Makefile
@@ -8,9 +8,13 @@ include $(DEPFILES)
 
 Bin/%: %.d | Bin Deps
 	ldc2 -i $< -betterC -fvisibility=hidden $(OPT) -of $@ -makedeps=Deps/$*.deps
+Bin/%-debug: %.d | Bin Deps
+	ldc2 -i $< -betterC -fvisibility=hidden -O0 -g -of $@ -makedeps=Deps/$*.deps
+Bin/%-release: %.d | Bin Deps
+	ldc2 -i $< -betterC -fvisibility=hidden -O3 -g -of $@ -makedeps=Deps/$*.deps
 
 .PHONY: ddasm
-ddasm: Bin/ddasm
+ddasm: Bin/ddasm Bin/ddasm-debug Bin/ddasm-release
 
 .PHONY: ds2dasm
 ds2dasm: Bin/ds2dasm
--- Samples/bench.c
+++ Samples/bench.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 void fizzother(FILE*, int);
-void main(){
+int main(){
     FILE* fp = fopen("/dev/null", "wb");
     for(int i = 10000; i; i = i - 1){
         fizzother(fp, 10000);
     }
     fflush(fp);
     fclose(fp);
+    return 0;
 }
 
 void fizzother(FILE* fp, int n){
-    int a = 1;
-    int mod_3 = 0;
-    int mod_5 = 0;
-    int d = n + 1;
-    int printed = 0;
+    size_t a = 1;
+    size_t mod_3 = 0;
+    size_t mod_5 = 0;
+    size_t d = n + 1;
+    size_t printed = 0;
     for(;a != d; a = a+1){
         printed = 0;
         mod_3 = mod_3 + 1;
