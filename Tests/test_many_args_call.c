dd719a50be3387b99200e6ccccad7e3045854c32
drpriver <david@davidpriver.com>
2026-01-02 18:31:27 -0800

    tests

---
+++ Tests/test_many_args_call.c
@@ -0,0 +1,11 @@
+int printf(const char* fmt, ...);
+
+long add8(long a, long b, long c, long d, long e, long f, long g, long h) {
+    return a + b + c + d + e + f + g + h;
+}
+
+int main() {
+    long result = add8(1, 2, 3, 4, 5, 6, 7, 8);
+    printf("sum=%ld\n", result);
+    return 0;
+}
