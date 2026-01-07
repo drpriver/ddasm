2ac832a1897c77c8af44dd74d6ab659eba4dcb48
drpriver <david@davidpriver.com>
2026-01-06 23:32:02 -0800

    expand test

--- Tests/call.c
+++ Tests/call.c
@@ -6,15 +6,16 @@ int add(int a, int b){
     return a + b;
 }
 
-void start(){
-    int x = add(1, 2);
-    if(x == 3){
-        printf("success\n");
-        exit(0);
-    }
-    else{
-        printf("fail\n");
-        exit(1);
-    }
+int addtwice(int a, int b){
+    return add(a, b) + add(a, b);
 }
-int main(){ return 0; }
+
+int main(){
+    int x = add(1, 2);
+    if(x != 3) return __LINE__;
+    int y = add(1, 2) + add(3, 4) + add(5, 6);
+    if(y != 21) return __LINE__;
+    int z = addtwice(7, 8);
+    if(z != 30) return __LINE__;
+    return 0;
+}
